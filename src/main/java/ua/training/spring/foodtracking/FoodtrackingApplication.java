5cc0b49536016e3fe83f425d1f4439acf1aa80b0
Andrew <andrew.kolomiyets@gmail.com>
2019-05-28 21:42:41 +0300

    Initial commit

---
+++ src/main/java/ua/training/spring/foodtracking/FoodtrackingApplication.java
@@ -0,0 +1,13 @@
+package ua.training.spring.foodtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodtrackingApplication.class, args);
+    }
+
+}
